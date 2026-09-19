0e9559911ffb5d4e1cb1e24745f8d3fd44eec0c7
reetumbhara <reetumbharap@gmail.com>
2026-09-19 18:54:25 -0400

    Revert "feat: add temporary comment"
    This reverts commit 3dfb5e1f15c4bb935457ddda978215b629de2c6b.

--- find_homologs.sh
+++ find_homologs.sh
@@ -3,4 +3,4 @@
  -query $1 \
  -subject $2 \
  -task tblastn \
- -outfmt '6 std sseq qlen' | awk '$3 > 30 && $4 > 0.9*$14' | tee $3 |wc -l# temporary line
+ -outfmt '6 std sseq qlen' | awk '$3 > 30 && $4 > 0.9*$14' | tee $3 |wc -l
